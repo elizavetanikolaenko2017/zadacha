af1c0ae4aab9958c43e347e0a158af18fac6f8bc
elizavetanikolaenko2017 <elizaveta.nikolaenko2017@gmail.com>
2023-03-24 15:35:05 +0300

    Задача

---
+++ zadacha01/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Написать программу,которя из имеющегося массива строк формирует массив из строк,длинна которого меньше,либо рравна 3 символа.
+*/
+
+int []array=new int[3];
+Random rand=new Random();
+for (int i = 0; i < array.Length; i++)
+{
+  array[i]=rand.Next(1,10);  
+}
+
+void FillArray(int[]arr)
+{
+  Random rand=new Random();
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i]= rand.Next(1,3);
+  }  
+}
+
+void PrintArray(int[]arr)
+{
+    Console.WriteLine("[" +string.Join(", ", array) + "]");
+}
+
+ int[]arr=new int[3];
+ FillArray(array);
+ PrintArray(array);
+
